5c2b1e9ba26043eb6ea60c17f864d8c028d0b1c6
Michael Watts <michael@amillionads.com>
2021-11-16 15:30:32 +0000

    initial commit with linear solution

---
+++ src/bruteForce.js
@@ -0,0 +1,26 @@
+const { isValidPermutation } = require("./isValidPermutation");
+
+const splitInteger = (x) => {
+    const padded = x.toString().padStart(5, "0");
+    const array = padded.split("");
+    const arrayOfNums = array.map(x => Number(x));
+    return arrayOfNums;
+};
+
+const bruteForce = () => {
+    const validPermutations = [];
+    const max = 99999;
+    let current = 0;
+
+    while (current <= max) {
+        if (isValidPermutation(splitInteger(current))) {
+            validPermutations.push(current);
+        }
+        current += 1;
+    }
+
+    return validPermutations;
+};
+
+const solutions = bruteForce();
+console.log(solutions);
